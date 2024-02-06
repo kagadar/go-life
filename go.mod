9f337f2a9695236c7f7743c602c4d73032fc72df
Harrison Short <kagadar@gmail.com>
2024-02-07 09:01:03 +1100

    add simple terminal renderer and load from stdin func

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/kagadar/go-life
 
 go 1.21.6
+
+require (
+	atomicgo.dev/cursor v0.2.0
+	github.com/kagadar/go-loop v0.0.0-20240206123048-79786c5f11e9
+	github.com/kagadar/go-pipeline v0.0.0-20240206075744-bd16111e7cd2
+)
--- main.go
+++ main.go
@@ -1,5 +1,53 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"os/signal"
+	"strings"
+	"time"
 
+	"atomicgo.dev/cursor"
+	"github.com/kagadar/go-loop"
+	"github.com/kagadar/go-pipeline/slices"
+)
+
+var (
+	tickrate = flag.Duration("main_loop_tick_delay", 16*time.Millisecond, "How long to wait between each tick of the main loop.")
+)
+
+func readInitialState(r io.Reader) [][]bool {
+	var board [][]bool
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		board = append(board, slices.Transform([]rune(scanner.Text()), func(r rune) bool {
+			return r == '■'
+		}))
+	}
+	return board
+}
+
+func main() {
+	board := readInitialState(os.Stdin)
+
+	fmt.Print("\033[2J")
+	defer cursor.Show()
+	cursor.Hide()
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+	loop.MainLoop(ctx, *tickrate, func(_ context.Context) {
+		fmt.Printf("\033[0;0H%s\n", strings.Join(slices.Transform(board, func(row []bool) string {
+			return string(slices.Transform(row, func(cell bool) rune {
+				if cell {
+					return '■'
+				} else {
+					return '□'
+				}
+			}))
+		}), "\n"))
+	})
 }
